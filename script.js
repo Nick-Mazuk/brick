f67ca92b30de3fd94459673abd7c8f6f200e845d
Nick Mazuk <trainridernick@gmail.com>
2015-05-01 11:47:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var openingScreen;
+var questionContainer;
 window.addEventListener("DOMContentLoaded", function() {start();});
 
 function start() {
 	openingScreen = document.getElementById("openingScreen");
+	questionContainer = document.getElementById("questionContainer");
 	openingScreen.addEventListener("click",function() {clickScreen();});
 }
 
 function clickScreen() {
 	openingScreen.style.display = "none";
+	questionContainer.style.display = "inline-flex";
 }
