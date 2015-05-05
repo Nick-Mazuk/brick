4e4fe09922953948a3f65cab88a9a71b7acd117d
Nick-Mazuk <trainridernick@gmail.com>
2015-05-04 21:45:01 -0700

    Got colors and end screen working functionally

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ var optionB;
 var optionC;
 var optionD;
 var userInput;
+var numQuestions = 4;
+var results;
+var questionIndex = 1;
 window.addEventListener("DOMContentLoaded", function() {start();});
 var questions = [
 	["If I were to give you a brick, what would you do?", "Throw it", "Grind it and make art", "Do nothing, it doesn't matter anyways"],
@@ -26,6 +29,7 @@ function start() {
 	optionC = document.getElementById("optionC");
 	optionD = document.getElementById("optionD");
 	userInput = document.getElementById("userInput");
+	results = document.getElementById("results");
 	optionA.addEventListener("click",function(){nextQuestion("a")});
 	optionB.addEventListener("click",function(){nextQuestion("b")});
 	optionC.addEventListener("click",function(){nextQuestion("c")});
@@ -36,39 +40,43 @@ function start() {
 function clickScreen() {
 	openingScreen.style.display = "none";
 	questionContainer.style.display = "inline-flex";
+	document.getElementById("progress").style.width = (questionNumber/numQuestions) * 100 + "%";
 }
 
 function nextQuestion(answer) {
-	if(questionNumber > questions.length - 1) {
+	if(answer == "a") {
+		answers.push([questionIndex, "A", optionA.innerHTML.substring(3)]);
+		personalityCount.push(0);
+		changeBackground(findMode(personalityCount));
+	} else if(answer == "b") {
+		answers.push([questionIndex, "B", optionB.innerHTML.substring(3)]);
+		personalityCount.push(1);
+		changeBackground(findMode(personalityCount));
+	} else if(answer == "c") {
+		answers.push([questionIndex, "C", optionC.innerHTML.substring(3)]);
+		personalityCount.push(2);
+		changeBackground(findMode(personalityCount));
+	} else if(answer == "d") {
+		answers.push([questionIndex, "D", userInput.value]);
+		personalityCount.push(3);
+		changeBackground(findMode(personalityCount));
+	}
+	if (questionNumber >= numQuestions || questionNumber > questions.length - 1) {
+		finished();
 		return null;
 	}
-	if(answer == "a") {
-		answers.push(["A", optionA.innerHTML.substring(3)]);
-		personalityCount.push(0);
-		changeBackground(findIndex(personalityCount));
-	} else if(answer == "b") {
-		answers.push(["B", optionB.innerHTML.substring(3)]);
-		personalityCount.push(1);
-		changeBackground(findIndex(personalityCount));
-	} else if(answer == "c") {
-		answers.push(["C", optionC.innerHTML.substring(3)]);
-		personalityCount.push(2);
-		changeBackground(findIndex(personalityCount));
-	} else if(answer == "d") {
-		answers.push(["D", userInput.value]);
-		personalityCount.push(3);
-		changeBackground(findIndex(personalityCount));
-	}
-	renderQuestion(questionNumber);
+	renderQuestion(questionIndex);
 }
 
 function renderQuestion(number) {
 	questionNumber++;
+	questionIndex++;
 	prompt.innerHTML = questionNumber +  ". " + questions[number][0];
 	optionA.innerHTML = "A. " + questions[number][1];
 	optionB.innerHTML = "B. " + questions[number][2];
 	optionC.innerHTML = "C. " + questions[number][3];
 	userInput.value = "";
+	document.getElementById("progress").style.width = (questionNumber/numQuestions) * 100 + "%";
 }
 
 function print2dArray(array) {
@@ -91,28 +99,49 @@ function printArray (array) {
 }
 
 function changeBackground (color) {
-	document.body.style.WebkitTransition = "backgroundColor " + (60/questionNumber) + "s";
-	document.body.style.MozTransition = "backgroundColor " + (60/questionNumber) + "s";
-	document.body.style.transition = "backgroundColor " + (60/questionNumber) + "s";
-	if(color == 0 && questionNumber > 4) {
-		document.body.style.backgroundColor = "#9999FF";
-	} else if (color == 1 && questionNumber > 4) {
-		document.body.style.backgroundColor = "#FF9999";
-	} else if (color == 2 && questionNumber > 4) {
-		document.body.style.backgroundColor = "#99FF99";
-	} else if (color == 3 && questionNumber > 4) {
-		document.body.style.backgroundColor = "#99FFFF";
+	if(color == 0 && questionNumber > 1) {
+		document.body.style.backgroundColor = "#FF3300";
+	} else if (color == 1 && questionNumber > 1) {
+		document.body.style.backgroundColor = "#00CC00";
+	} else if (color == 2 && questionNumber > 1) {
+		document.body.style.backgroundColor = "#0066FF";
+	} else if (color == 3 && questionNumber > 1) {
+		document.body.style.backgroundColor = "#FFFF00";
 	}
 }
 
-function findIndex(array) {
-	var max = array[0];
-	var maxIndex = 0;
-	for (var i = 1; i < array.length; i++) {
-	    if (array[i] > max) {
-	        maxIndex = i;
-	        max = array[i];
-	    }
+function findMode(array) {
+	if(array.length == 0)
+    	return null;
+    var modeMap = {};
+    var maxEl = array[0], maxCount = 1;
+    for(var i = 0; i < array.length; i++)
+    {
+    	var el = array[i];
+    	if(modeMap[el] == null)
+    		modeMap[el] = 1;
+    	else
+    		modeMap[el]++;	
+    	if(modeMap[el] > maxCount)
+    	{
+    		maxEl = el;
+    		maxCount = modeMap[el];
+    	}
+    }
+    return maxEl;
+}
+
+function finished() {
+	questionContainer.style.display = "none";
+	results.style.display = "block";
+	for(i = 0; i < answers.length; i++) {
+		showQuestion(answers[i]);
 	}
-	return maxIndex;
+}
+
+function showQuestion(array) {
+	var output = "<div class='question'><h3>" + (array[0]) + ". ";
+	output += questions[array[0]-1][0] + "</h3>";
+	output += "<p>" + array[1] + ". " + array[2] + "</p></div>";
+	results.innerHTML += output;
 }
